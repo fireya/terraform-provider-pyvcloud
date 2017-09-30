41df2476ba9fa2c2cb0928dfbe6ce0e13e22c769
SRI NARAYANAN T <srinarayana@srinarayana-m01.vmware.com>
2017-09-30 23:29:28 +0530

    vcd

---
+++ vcd/client.go
@@ -0,0 +1,59 @@
+package vcd
+
+import (
+	"fmt"
+//	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/hashicorp/go-plugin"
+
+	"../goclient/shared"
+	
+)
+
+func Login() {
+	// We don't want to see the plugin logs.
+	//log.SetOutput(ioutil.Discard)
+	log.SetOutput(os.Stdout)
+	
+
+	// We're a host. Start by launching the plugin process.
+	client := plugin.NewClient(&plugin.ClientConfig{
+		HandshakeConfig: shared.Handshake,
+		Plugins:         shared.PluginMap,
+		Cmd:             exec.Command("sh", "-c", os.Getenv("PY_PLUGIN")),
+		AllowedProtocols: []plugin.Protocol{
+			plugin.ProtocolNetRPC, plugin.ProtocolGRPC},
+	})
+	defer client.Kill()
+
+	// Connect via RPC
+	rpcClient, err := client.Client()
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		os.Exit(1)
+	}
+
+	// Request the plugin
+	raw, err := rpcClient.Dispense("PY_PLUGIN")
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		os.Exit(1)
+	}
+
+	// We should have a KV store now! This feels like a normal interface
+	// implementation but is in fact over an RPC connection.
+	kv := raw.(shared.PyVcloudProvider)
+
+	result, err := kv.Login("user1","pass","O1")
+
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(string(result.Token))
+
+}
+
