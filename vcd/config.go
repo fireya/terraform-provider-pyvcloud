dc4d9b100fc59d322e1d790276f2b7029dc335e1
sri.narayanan <sri.narayanan.t@gmail.com>
2017-09-30 17:55:04 +0000

    vcdcli

---
+++ vcd/config.go
@@ -0,0 +1,25 @@
+package vcd
+
+//import (
+//	"fmt"
+//	"net/url"
+//)
+
+type Config struct {
+	User            string
+	Password        string
+	Org             string
+	Href            string
+	VDC             string
+	MaxRetryTimeout int
+	InsecureFlag    bool
+}
+
+type VCDClient struct {
+}
+
+func (c *Config) Client() (*VCDClient, error) {
+
+	vcdclient := &VCDClient{}
+	return vcdclient, nil
+}
